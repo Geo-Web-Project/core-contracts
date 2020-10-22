7fe5efbc2d0c7e9f948cb0cb74973f1995af790b
Cody Hatfield <cody.hatfield@me.com>
2020-10-22 14:35:49 -0700

    Lots of gas optimizations

--- scripts/estimate_minting_gas.js
+++ scripts/estimate_minting_gas.js
@@ -7,6 +7,38 @@ function makePathPrefix(length) {
     return new BN(length).shln(256-8)
 }
 
+async function traverseSingle() {
+    let gwCoor = await GeoWebCoordinate.deployed();
+
+    let direction = (new BN('00', 2))
+
+    let gas = await gwCoor.traverse.estimateGas(new BN('0', 2), direction, new BN('0', 2), new BN('0', 2), new BN(0))
+
+    console.log(`Estimated gas for single traverse: ${gas-21000}`)
+}
+
+async function parseDirection() {
+    let gwCoor = await GeoWebCoordinatePath.deployed();
+
+    let path = (new BN(2)).shln(256-8).or(new BN('1110', 2))
+
+    let gas = await gwCoor.nextDirection.estimateGas(path)
+
+    console.log(`Estimated gas for parsing direction: ${gas-21000}`)
+}
+
+async function wordIndex() {
+    let gwCoor = await GeoWebCoordinate.deployed();
+
+    // Global(4, 33) -> Index(0, 2), Local(4, 1)
+    let coord = (new BN(4)).shln(32).or(new BN(33))
+
+    let gas = await gwCoor.toWordIndex.estimateGas(coord)
+
+    console.log(`Estimated gas for word index: ${gas-21000}`)
+}
+
+
 async function mintSingleCoordinate() {
     let GW = await GeoWebParcel.deployed()
     
@@ -18,15 +50,27 @@ async function mintSingleCoordinate() {
     console.log(`Estimated gas for single coordinate mint: ${gas}`)
 }
 
-async function mintSingleWord() {
+async function mintPath(count) {
+    let GW = await GeoWebParcel.deployed()
+    
+    // Global(160000, 17) -> Index(100000, 1), Local(0, 1)
+    let coord = (new BN(160000)).shln(32).or(new BN(17))
+
+    let gas = await GW.mintLandParcel.estimateGas(coord, [makePathPrefix(count)])
+
+    console.log(`Estimated gas for ${count} path mint: ${gas}`)
+}
+
+async function mintSingleWord(count) {
     let GW = await GeoWebParcel.deployed()
     let GWC = await GeoWebCoordinate.deployed()
     
     // Global(160000, 17) -> Index(100000, 1), Local(0, 1)
     let coord = (new BN(160000)).shln(32).or(new BN(0))
 
+    var paths = []
     var path = new BN(0)
-    for (let i = 1; i < 100; i++) {
+    for (let i = 1; i < count; i++) {
         var direction
         if (i % 16 == 0) {
             // North
@@ -39,20 +83,28 @@ async function mintSingleWord() {
             direction = new BN('11', 2)
         }
         path = direction.shln(i*2).or(path.shrn(2))
-    }
-    let gas = await GW.mintLandParcel.estimateGas(coord, [makePathPrefix(99).or(path)])
 
-    console.log(`Estimated gas for 1 word mint of 100 coordinates: ${gas}`)
+        if (i % 124 == 0) {
+            paths.push(makePathPrefix(124).or(path))
+            path = new BN(0)
+        }
+    }
+    
+    paths.push(makePathPrefix((count%124)-1).or(path))
+    let gas = await GW.mintLandParcel.estimateGas(coord, paths)
+
+    console.log(`Estimated gas for 1 word mint of ${count} coordinates: ${gas}`)
 }
 
-async function mintMultipleWord() {
+async function mintMultipleWord(count) {
     let GW = await GeoWebParcel.deployed()
     
     // Global(159999, 15) -> Index(99999, 15), Local(15, 15)
     let coord = (new BN(159999)).shln(32).or(new BN(15))
 
+    var paths = []
     var path = new BN(0)
-    for (let i = 1; i < 100; i++) {
+    for (let i = 1; i < count; i++) {
         var direction
         if (i % 16 == 0) {
             // North
@@ -65,16 +117,32 @@ async function mintMultipleWord() {
             direction = new BN('11', 2)
         }
         path = direction.shln(i*2).or(path.shrn(2))
-    }
-    let gas = await GW.mintLandParcel.estimateGas(coord, [makePathPrefix(99).or(path)])
 
-    console.log(`Estimated gas for 4 word mint of 100 coordinates: ${gas}`)
+        if (i % 124 == 0) {
+            paths.push(makePathPrefix(124).or(path))
+            path = new BN(0)
+        }
+    }
+    
+    paths.push(makePathPrefix((count%124)-1).or(path))
+    let gas = await GW.mintLandParcel.estimateGas(coord, paths)
+
+    console.log(`Estimated gas for 4 word mint of ${count} coordinates: ${gas}`)
 }
 
 async function r() {
+    await traverseSingle()
+    await parseDirection()
+    await wordIndex()
     await mintSingleCoordinate()
-    await mintSingleWord()
-    await mintMultipleWord()
+    await mintPath(1)
+    await mintPath(2)
+    await mintSingleWord(10)
+    await mintMultipleWord(10)
+    await mintSingleWord(100)
+    await mintMultipleWord(100)
+    await mintSingleWord(1000)
+    await mintMultipleWord(1000)
 }
 
 module.exports = function(callback) {
