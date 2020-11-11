6a7d1696986921a66354fdc819e8aa601242103e
Cody Hatfield <cody.hatfield@me.com>
2020-11-11 12:15:04 -0800

    Update admin contract migration to reuse contracts

--- migrations/2_admin.js
+++ migrations/2_admin.js
@@ -1,5 +1,7 @@
 const GeoWebAdmin = artifacts.require("GeoWebAdmin");
 const ERC20Mock = artifacts.require("ERC20Mock");
+const ERC721License = artifacts.require("ERC721License");
+const GeoWebParcel = artifacts.require("GeoWebParcel");
 
 function perYearToPerSecondRate(annualRate) {
   return {
@@ -12,12 +14,23 @@ module.exports = function (deployer, network, accounts) {
   return deployer.deploy(ERC20Mock).then(async (erc20Mock) => {
     let rate = perYearToPerSecondRate(0.1);
 
-    await deployer.deploy(
+    let adminContract = await deployer.deploy(
       GeoWebAdmin,
       erc20Mock.address,
       web3.utils.toWei("10"),
       rate.numerator,
       rate.denominator
     );
+
+    // Migrate license and parcel contracts if deployed
+    let licenseContract = await ERC721License.deployed();
+    if (licenseContract) {
+      await adminContract.setLicenseContract(licenseContract.address);
+    }
+
+    let parcelContract = await GeoWebParcel.deployed();
+    if (parcelContract) {
+      await adminContract.setParcelContract(parcelContract.address);
+    }
   });
 };
