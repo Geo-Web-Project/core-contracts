62692ce2f217170bc0a5f95bda4c41f464a531b9
Cody Hatfield <cody.hatfield@me.com>
2021-07-29 15:40:59 -0700

    Migrate Truffle config, test deploy to Kovan

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,5 +6,33 @@ require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
 
 module.exports = {
-  solidity: "0.6.2",
+  networks: {
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.DEV_PRIVATE_KEY],
+      chainId: 0x2a,
+      gas: 4700000,
+    },
+    sokul: {
+      url: "https://sokol.poa.network",
+      accounts: [process.env.DEV_PRIVATE_KEY],
+      chainId: 77,
+      gasPrice: 1000000000,
+    },
+    xdai: {
+      url: "https://xdai.poanetwork.dev",
+      accounts: [process.env.DEV_PRIVATE_KEY],
+      network_id: 100,
+      gasPrice: 1000000000,
+    },
+  },
+  solidity: {
+    version: "0.6.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
