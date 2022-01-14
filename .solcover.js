2e3815323ffbeaf373f4ae48ee9dcfe069b21c4d
Cody Hatfield <cody.hatfield@me.com>
2022-01-13 18:07:15 -0800

    Add test coverage

--- .solcover.js
+++ .solcover.js
@@ -2,5 +2,6 @@ module.exports = {
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
     invert: true, // Run the grep's inverse set.
+    enableTimeouts: false,
   },
 };
