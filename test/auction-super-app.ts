8ad8a3156fb2504c4acb48177cde09b59961ef00
Cody Hatfield <cody.hatfield@me.com>
2022-01-19 17:27:49 -0800

    Setup AuctionSuperApp test fixtures

--- test/auction-super-app.ts
+++ test/auction-super-app.ts
@@ -3,7 +3,8 @@ import * as chai from "chai";
 var chaiAsPromised = require("chai-as-promised");
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers, web3 } from "hardhat";
-import { Framework } from "@superfluid-finance/sdk-core";
+import { Framework, SuperToken } from "@superfluid-finance/sdk-core";
+import { Contract } from "ethers";
 const SuperfluidSDK = require("@superfluid-finance/js-sdk");
 const BigNumber = ethers.BigNumber;
 const deployFramework = require("@superfluid-finance/ethereum-contracts/scripts/deploy-framework");
@@ -66,6 +67,60 @@ describe("AuctionSuperApp", async () => {
   })
 
   describe("No current owner bid", async () => {
+    let ethx: SuperToken;
+    let ethersjsSf: Framework;
+    let superApp: Contract;
+    let user: SignerWithAddress;
+
+    beforeEach(async () => {
+      await deploySuperToken(errorHandler, [":", "ETH"], {
+          web3,
+          from: accounts[0].address
+      });
+
+      const sf = new SuperfluidSDK.Framework({
+          web3,
+          version: "test",
+          tokens: ["ETH"],
+      });
+      await sf.initialize();
+
+      const ethersProvider = accounts[0].provider!;
+      ethersjsSf = await Framework.create({
+          networkName: "custom",
+          dataMode: "WEB3_ONLY",
+          resolverAddress: sf.resolver.address,
+          protocolReleaseVersion: "test",
+          provider: ethersProvider
+      });
+
+      user = accounts[1];
+
+      ethx = await ethersjsSf.loadSuperToken(sf.tokens.ETHx.address);
+
+      await sf.tokens.ETHx.upgradeByETH({
+        from: user.address,
+        value: ethers.utils.parseEther("10")
+      });
+
+      superApp = await buildAuctionSuperApp({
+          host: sf.host.address,
+          cfa: sf.agreements.cfa.address,
+          token: sf.tokens.ETHx.address,
+          receiver: accounts[0].address
+      });
+    });
+
+    it("should claim on flow increase", async () => {
+      const createFlowOperation = await ethersjsSf.cfaV1.createFlow({
+          sender: user.address, 
+          receiver: superApp.address,
+          flowRate: "100",
+          superToken: ethx.address
+      });
+      const txnResponse = await createFlowOperation.exec(accounts[1]);
+      await txnResponse.wait();
+    })
   })
 
 //   it("should create Flow(app -> user) equal to Flow(user -> app) on agreement created", async () => {
