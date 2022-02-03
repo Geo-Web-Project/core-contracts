3e7582ed5a7ba4fa0762209329922e3577d30b3a
Brian Rossetti <brossetti1@gmail.com>
2022-02-02 18:19:37 -0700

    exporting contracts from index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { Accountant__factory as AccountantFactory } from '../typechain-types/factories/Accountant__factory';
+export { ERC721License__factory as ERC721LicenseFactory } from '../typechain-types/factories/ERC721License__factory';
+export { ETHExpirationCollector__factory as ETHExpirationCollectorFactory } from '../typechain-types/factories/ETHExpirationCollector__factory';
+export { ETHPurchaser__factory as ETHPurchaserFactory } from '../typechain-types/factories/ETHPurchaser__factory';
+export { GeoWebCoordinate__factory as GeoWebCoordinateFactory } from '../typechain-types/factories/GeoWebCoordinate__factory';
+export { GeoWebParcel__factory as GeoWebParcelFactory } from '../typechain-types/factories/GeoWebParcel__factory';
+export { SimpleETHClaimer__factory as SimpleETHClaimerFactory } from '../typechain-types/factories/SimpleETHClaimer__factory';
