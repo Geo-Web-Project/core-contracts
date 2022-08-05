8dfa3960b84642b4dcde3c403e181298bb12c725
Cody Hatfield <cody.hatfield@me.com>
2022-08-05 16:25:14 -0700

    Comment out reclaimer stub

--- contracts/pco-license/facets/CFAReclaimerFacet.sol
+++ contracts/pco-license/facets/CFAReclaimerFacet.sol
@@ -8,21 +8,21 @@ import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/app
 contract CFAReclaimerFacet is CFABasePCOFacetModifiers {
     using CFAv1Library for CFAv1Library.InitData;
 
-    /**
-     * @notice Whether reclaiming is currently active
-     */
-    function isReclaimingActive() view returns (bool) {}
+    // /**
+    //  * @notice Whether reclaiming is currently active
+    //  */
+    // function isReclaimingActive() view returns (bool) {}
 
-    /**
-     * @notice Current price to reclaim
-     */
-    function claimPrice() view returns (uint256) {}
+    // /**
+    //  * @notice Current price to reclaim
+    //  */
+    // function claimPrice() view returns (uint256) {}
 
-    /**
-     * @notice Reclaim an inactive license as msg.sender
-     *      - Payer bid must be inactive
-     *      - Must have permissions to create flow for bidder
-     *      - Must have ERC-20 approval of payment token for claimPrice amount
-     */
-    function claim() external {}
+    // /**
+    //  * @notice Reclaim an inactive license as msg.sender
+    //  *      - Payer bid must be inactive
+    //  *      - Must have permissions to create flow for bidder
+    //  *      - Must have ERC-20 approval of payment token for claimPrice amount
+    //  */
+    // function claim() external {}
 }
