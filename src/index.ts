87a0c5ef565c0403ab93842b73629c08de59912d
Cody Hatfield <cody.hatfield@me.com>
2022-08-17 16:51:34 -0700

    feat: Export factories and ABI

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-// export { default as ERC721LicenseABI } from "../abi/contracts/ERC721License.sol/ERC721License.json";
-// export { default as GeoWebCoordinateABI } from "../abi/contracts/GeoWebCoordinate.sol/GeoWebCoordinate.json";
-// export { default as GeoWebParcelABI } from "../abi/contracts/GeoWebParcel.sol/GeoWebParcel.json";
-// export { default as AuctionSuperAppABI } from "../abi/contracts/AuctionSuperApp.sol/AuctionSuperApp.json";
-// export { default as FairLaunchClaimerABI } from "../abi/contracts/FairLaunchClaimer.sol/FairLaunchClaimer.json";
-// export { default as ReclaimerABI } from "../abi/contracts/Reclaimer.sol/Reclaimer.json";
+export { default as ERC721FacetABI } from "../abi/contracts/registry/facets/ERC721Facet.sol/ERC721Facet.json";
+export { default as GeoWebParcelFacetABI } from "../abi/contracts/registry/facets/GeoWebParcelFacet.sol/GeoWebParcelFacet.json";
+export { default as PCOLicenseClaimerFacetABI } from "../abi/contracts/registry/facets/PCOLicenseClaimerFacet.sol/PCOLicenseClaimerFacet.json";
+export { default as PCOLicenseParamsFacetABI } from "../abi/contracts/registry/facets/PCOLicenseParamsFacet.sol/PCOLicenseParamsFacet.json";
+export { default as CFABasePCOFacetABI } from "../abi/contracts/pco-license/facets/CFABasePCOFacet.sol/CFABasePCOFacet.json";
+export { default as CFAPenaltyBidFacetABI } from "../abi/contracts/pco-license/facets/CFAPenaltyBidFacet.sol/CFAPenaltyBidFacet.json";
 
-// export { ERC721License__factory as ERC721LicenseFactory } from "../typechain-types/factories/ERC721License__factory";
-// export { GeoWebCoordinate__factory as GeoWebCoordinateFactory } from "../typechain-types/factories/GeoWebCoordinate__factory";
-// export { GeoWebParcel__factory as GeoWebParcelFactory } from "../typechain-types/factories/GeoWebParcel__factory";
-// export { AuctionSuperApp__factory as AuctionSuperAppFactory } from "../typechain-types/factories/AuctionSuperApp__factory";
-// export { FairLaunchClaimer__factory as FairLaunchClaimerFactory } from "../typechain-types/factories/FairLaunchClaimer__factory";
-// export { Reclaimer__factory as ReclaimerFactory } from "../typechain-types/factories/Reclaimer__factory";
+export { ERC721Facet__factory as ERC721FacetFactory } from "../typechain-types/factories/ERC721Facet__factory";
+export { GeoWebParcelFacet__factory as GeoWebParcelFacetFactory } from "../typechain-types/factories/GeoWebParcelFacet__factory";
+export { PCOLicenseClaimerFacet__factory as PCOLicenseClaimerFacetFactory } from "../typechain-types/factories/PCOLicenseClaimerFacet__factory";
+export { PCOLicenseParamsFacet__factory as PCOLicenseParamsFacetFactor } from "../typechain-types/factories/PCOLicenseParamsFacet__factory";
+export { CFABasePCOFacet__factory as CFABasePCOFacetFactory } from "../typechain-types/factories/CFABasePCOFacet__factory";
+export { CFAPenaltyBidFacet__factory as CFAPenaltyBidFacetFactory } from "../typechain-types/factories/CFAPenaltyBidFacet__factory";
