c4dac0de139d4d9a422b20d10cdf4c80ac0e0e6c
lolboysg <mateo.viel@epitech.eu>
2022-08-28 02:11:17 +0200

    feat: claimPrice for reclaimer faceit

--- contracts/pco-license/facets/CFAReclaimerFacet.sol
+++ contracts/pco-license/facets/CFAReclaimerFacet.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "../libraries/LibCFABasePCO.sol";
 import {CFABasePCOFacetModifiers} from "./CFABasePCOFacet.sol";
 import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
 
@@ -8,21 +9,81 @@ import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/app
 contract CFAReclaimerFacet is CFABasePCOFacetModifiers {
     using CFAv1Library for CFAv1Library.InitData;
 
-    // /**
-    //  * @notice Whether reclaiming is currently active
-    //  */
-    // function isReclaimingActive() view returns (bool) {}
+    /// @notice Emitted when a license is reclaimed
+    event LicenseReclaimed(address indexed to, uint256 price);
 
-    // /**
-    //  * @notice Current price to reclaim
-    //  */
-    // function claimPrice() view returns (uint256) {}
+    /**
+     * @notice Current price to reclaim
+     */
+    function claimPrice() public view returns (uint256) {
+        LibCFABasePCO.DiamondStorage storage ds = LibCFABasePCO
+            .diamondStorage();
+        LibCFABasePCO.DiamondCFAStorage storage cs = LibCFABasePCO.cfaStorage();
 
-    // /**
-    //  * @notice Reclaim an inactive license as msg.sender
-    //  *      - Payer bid must be inactive
-    //  *      - Must have permissions to create flow for bidder
-    //  *      - Must have ERC-20 approval of payment token for claimPrice amount
-    //  */
-    // function claim() external {}
+        LibCFABasePCO.Bid storage _currentBid = LibCFABasePCO._currentBid();
+
+        uint256 originalForSalePrice = _currentBid.forSalePrice;
+        (uint256 startTime, , , ) = cs.cfaV1.cfa.getFlow(
+            ds.paramsStore.getPaymentToken(),
+            address(this),
+            ds.paramsStore.getBeneficiary()
+        );
+        uint256 _length = ds.paramStore.getReclaimAuctionLength();
+
+        if (block.timestamp > startTime + _length) {
+            return 0;
+        }
+
+        uint256 timeElapsed = block.timestamp - startTime;
+        uint256 priceDecrease = (originalForSalePrice * timeElapsed) / _length;
+        return originalForSalePrice - priceDecrease;
+    }
+
+    /**
+     * @notice Reclaim an inactive license as msg.sender
+     *      - Payer bid must be inactive
+     *      - Must have permissions to create flow for bidder
+     *      - Must have ERC-20 approval of payment token for claimPrice amount
+     */
+    function claim() external {
+        LibCFABasePCO.DiamondStorage storage ds = LibCFABasePCO
+            .diamondStorage();
+
+        require(
+            LibCFABasePCO._isPayerBidActive() == false,
+            "CFAReclaimerFacet: Payer bid must be inactive"
+        );
+
+        /*
+
+        // Check operator permissions
+        (, uint8 permissions, int96 flowRateAllowance) = cs
+            .cfaV1
+            .cfa
+            .getFlowOperatorData(
+                ds.paramsStore.getPaymentToken(),
+                msg.sender,
+                address(this)
+            );
+
+        require(
+            LibCFAPenaltyBid._getBooleanFlowOperatorPermissions(
+                permissions,
+                LibCFAPenaltyBid.FlowChangeType.CREATE_FLOW
+            ),
+            "CFAReclaimerFacet: CREATE_FLOW permission not granted"
+        );
+
+        // Collect deposit
+        ISuperToken paymentToken = ds.paramsStore.getPaymentToken();
+        bool success = paymentToken.transferFrom(
+            msg.sender,
+            ds.paramsStore.getBeneficiary(),
+            claimPrice()
+        );
+        require(success, "CFAPenaltyBidFacet: Bid deposit failed");
+
+        */
+        emit LicenseReclaimed(msg.sender, claimPrice());
+    }
 }
