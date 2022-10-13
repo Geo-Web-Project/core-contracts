4eb6bd661cfdcad1dea6902e96d27c9887dd5cda
Cody Hatfield <cody.hatfield@me.com>
2022-10-12 18:31:16 -0700

    Add multisig initialization

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,65 +1,22 @@
-import SuperfluidSDK from "@superfluid-finance/js-sdk";
-const deployFramework = require("@superfluid-finance/ethereum-contracts/scripts/deploy-framework");
-const deploySuperToken = require("@superfluid-finance/ethereum-contracts/scripts/deploy-super-token");
-import { Framework, SuperToken } from "@superfluid-finance/sdk-core";
 const hre = require("hardhat");
-import { Contract } from "ethers";
-import { ethers, upgrades } from "hardhat";
-const { getSelectors, FacetCutAction } = require("./libraries/diamond.js");
+import { Contract, Signer } from "ethers";
+import { ethers } from "hardhat";
 import { DiamondWritable } from "../typechain-types/DiamondWritable";
 import { Ownable } from "../typechain-types/Ownable";
-
-function perYearToPerSecondRate(annualRate: number) {
-  return {
-    numerator: annualRate * 100,
-    denominator: 60 * 60 * 24 * 365 * 100,
-  };
-}
-
-async function deploySuperfluid() {
-  const errorHandler = (err: any) => {
-    if (err) throw err;
-  };
-
-  const { diamondAdmin } = await hre.getNamedAccounts();
-
-  await deployFramework(errorHandler, {
-    web3: hre.web3,
-    from: diamondAdmin,
-  });
-
-  await deploySuperToken(errorHandler, [":", "ETH"], {
-    web3: hre.web3,
-    from: diamondAdmin,
-  });
-
-  const jsSf = new SuperfluidSDK.Framework({
-    web3: hre.web3,
-    version: "test",
-    tokens: ["ETH"],
-  });
-  await jsSf.initialize();
-
-  const sf = await Framework.create({
-    chainId: hre.network.config.chainId,
-    provider: hre.web3,
-    resolverAddress: (jsSf.resolver as Contract).address,
-    protocolReleaseVersion: "test",
-  });
-
-  const ethx = await sf.loadSuperToken(jsSf.tokens.ETHx.address);
-
-  return { sf, ethx };
-}
+const { getSelectors, FacetCutAction } = require("./libraries/diamond.js");
 
 export async function deployDiamond(
   name: string,
-  { facets, from, owner }: { facets: string[]; from: string; owner: string }
+  {
+    facets,
+    from,
+    owner,
+  }: { facets: string[]; from: string | Signer; owner: string }
 ) {
+  from = typeof from === "string" ? await ethers.getSigner(from) : from;
+
   const DiamondInit = await ethers.getContractFactory(name);
-  const diamondInit = await DiamondInit.connect(
-    await ethers.getSigner(from)
-  ).deploy();
+  const diamondInit = await DiamondInit.connect(from).deploy();
   await diamondInit.deployed();
   console.log(`${name} deployed:`, diamondInit.address);
 
@@ -70,7 +27,7 @@ export async function deployDiamond(
   const facetCuts = [];
   for (const FacetName of facets) {
     const Facet = await ethers.getContractFactory(FacetName);
-    const facet = await Facet.connect(await ethers.getSigner(from)).deploy();
+    const facet = await Facet.connect(from).deploy();
     await facet.deployed();
     console.log(`${FacetName} deployed: ${facet.address}`);
     facetCuts.push({
@@ -85,37 +42,17 @@ export async function deployDiamond(
 
   // Cut diamond
   await (diamondInit as DiamondWritable)
-    .connect(await ethers.getSigner(from))
+    .connect(from)
     .diamondCut(facetCuts, target, data);
   console.log();
   console.log("Diamond deployed:", diamondInit.address);
 
   // Set owner
-  if (from !== owner) {
-    await (diamondInit as Ownable)
-      .connect(await ethers.getSigner(from))
-      .transferOwnership(owner);
+  if ((await from.getAddress()) !== owner) {
+    await (diamondInit as Ownable).connect(from).transferOwnership(owner);
   }
 
   return await ethers.getContractAt(`${name}ABI`, diamondInit.address);
-}
-
-async function deployBeneficiarySuperApp(registryDiamond: Contract) {
-  const { treasury } = await hre.getNamedAccounts();
-
-  console.log();
-  console.log("Deploying BeneficiarySuperApp");
-  const BeneficiarySuperApp = await ethers.getContractFactory(
-    "BeneficiarySuperApp"
-  );
-  const beneSuperApp = await upgrades.deployProxy(BeneficiarySuperApp, [
-    registryDiamond.address,
-    treasury,
-  ]);
-  await beneSuperApp.deployed();
-  console.log("BeneficiarySuperApp deployed: ", beneSuperApp.address);
-
-  return beneSuperApp;
 }
 
 async function deployBeaconDiamond() {
@@ -133,10 +70,10 @@ async function deployBeaconDiamond() {
   return beaconDiamond;
 }
 
-async function deployRegistryDiamond(sf: Framework, ethx: SuperToken) {
-  const { diamondAdmin, deployer, treasury } = await hre.getNamedAccounts();
+async function deployRegistryDiamond() {
+  const { diamondAdmin, deployer } = await hre.getNamedAccounts();
 
-  const beaconDiamond = await deployBeaconDiamond();
+  await deployBeaconDiamond();
 
   console.log();
   console.log("Deploying RegistryDiamond");
@@ -151,47 +88,10 @@ async function deployRegistryDiamond(sf: Framework, ethx: SuperToken) {
     from: deployer,
   });
   console.log("RegistryDiamond deployed: ", registryDiamond.address);
-
-  // Initialize
-  const perSecondFee = perYearToPerSecondRate(0.1);
-
-  await registryDiamond.initializeERC721("Geo Web Parcel License", "GEOL", "");
-  await registryDiamond.initializeClaimer(0, 0, 0, 0, beaconDiamond.address);
-  await registryDiamond.initializeParams(
-    treasury,
-    ethx.address,
-    sf.host.contract.address,
-    perSecondFee.numerator,
-    perSecondFee.denominator,
-    1,
-    10,
-    60 * 60 * 24 * 7, // 7 days
-    60 * 60 * 24 * 14, // 2 weeks,
-    ethers.utils.parseEther("0.005")
-  );
-  console.log("Initialized RegistryDiamond.");
-
-  const beneSuperApp = await deployBeneficiarySuperApp(registryDiamond);
-  console.log("Setting beneficiary to super app...");
-  await registryDiamond.setBeneficiary(beneSuperApp.address);
 }
 
 async function deploy() {
-  let sf: Framework;
-  let ethx: SuperToken;
-  if (hre.network.config.chainId == 31337) {
-    const res = await deploySuperfluid();
-    sf = res.sf;
-    ethx = res.ethx;
-  } else {
-    sf = await Framework.create({
-      chainId: hre.network.config.chainId!,
-      provider: hre.ethers.provider,
-    });
-    ethx = await sf.loadSuperToken("ETHx");
-  }
-
-  await deployRegistryDiamond(sf, ethx);
+  await deployRegistryDiamond();
 }
 
 if (require.main === module) {
---
+++ tasks/initialize.ts
@@ -0,0 +1,204 @@
+import { Contract } from "ethers";
+import { BeneficiarySuperApp } from "../typechain-types/BeneficiarySuperApp";
+import {
+  RegistryDiamondABI__factory,
+  SafeOwnable__factory,
+} from "../typechain-types";
+import { SafeEthersSigner, SafeService } from "@gnosis.pm/safe-ethers-adapters";
+import EthersAdapter from "@gnosis.pm/safe-ethers-lib";
+import Safe from "@gnosis.pm/safe-core-sdk";
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import SuperfluidSDK from "@superfluid-finance/js-sdk";
+const deployFramework = require("@superfluid-finance/ethereum-contracts/scripts/deploy-framework");
+const deploySuperToken = require("@superfluid-finance/ethereum-contracts/scripts/deploy-super-token");
+import { Framework, SuperToken } from "@superfluid-finance/sdk-core";
+
+const SAFE_SERVICES: Record<number, string> = {
+  5: "https://safe-transaction.goerli.gnosis.io/",
+};
+
+function perYearToPerSecondRate(annualRate: number) {
+  return {
+    numerator: annualRate * 100,
+    denominator: 60 * 60 * 24 * 365 * 100,
+  };
+}
+
+export async function deploySuperfluid(hre: HardhatRuntimeEnvironment) {
+  const errorHandler = (err: any) => {
+    if (err) throw err;
+  };
+
+  const { diamondAdmin } = await hre.getNamedAccounts();
+
+  await deployFramework(errorHandler, {
+    web3: hre.web3,
+    from: diamondAdmin,
+  });
+
+  await deploySuperToken(errorHandler, [":", "ETH"], {
+    web3: hre.web3,
+    from: diamondAdmin,
+  });
+
+  const jsSf = new SuperfluidSDK.Framework({
+    web3: hre.web3,
+    version: "test",
+    tokens: ["ETH"],
+  });
+  await jsSf.initialize();
+
+  const sf = await Framework.create({
+    chainId: hre.network.config.chainId!,
+    provider: hre.web3,
+    resolverAddress: (jsSf.resolver as Contract).address,
+    protocolReleaseVersion: "test",
+  });
+
+  const ethx = await sf.loadSuperToken(jsSf.tokens.ETHx.address);
+
+  return { sf, ethx };
+}
+
+async function deployBeneficiarySuperApp(
+  hre: HardhatRuntimeEnvironment,
+  registryDiamond: Contract
+) {
+  const { treasury, diamondAdmin } = await hre.getNamedAccounts();
+
+  console.log();
+  console.log("Deploying BeneficiarySuperApp");
+  const factory = await hre.ethers.getContractFactory("BeneficiarySuperApp");
+  const beneSuperApp = (await hre.upgrades.deployProxy(factory, [
+    registryDiamond.address,
+    treasury,
+  ])) as BeneficiarySuperApp;
+  await beneSuperApp.deployed();
+  console.log("BeneficiarySuperApp deployed: ", beneSuperApp.address);
+
+  // Set owner
+  const txn = await beneSuperApp.transferOwnership(diamondAdmin);
+  console.log("Waiting to sign:", txn.hash);
+  await txn.wait();
+
+  return beneSuperApp;
+}
+
+async function initializeRegistryDiamond(
+  hre: HardhatRuntimeEnvironment,
+  sf: Framework,
+  ethx: SuperToken,
+  registryDiamondAddress: string,
+  beaconDiamondAddress: string
+) {
+  const { diamondAdmin, deployer, treasury } = await hre.getNamedAccounts();
+
+  // Initialize with Safe signer
+  const service = new SafeService(
+    SAFE_SERVICES[hre.network.config.chainId as number]
+  );
+  const ethAdapter = new EthersAdapter({
+    ethers: hre.ethers as any,
+    signer: (await hre.ethers.getSigner(deployer)) as any,
+  });
+  const safe = await Safe.create({
+    ethAdapter,
+    safeAddress: diamondAdmin,
+  });
+  const safeSigner = new SafeEthersSigner(
+    safe,
+    service,
+    hre.network.provider as any
+  );
+
+  const registryDiamond = RegistryDiamondABI__factory.connect(
+    registryDiamondAddress,
+    safeSigner
+  );
+  const ownable = SafeOwnable__factory.connect(
+    registryDiamondAddress,
+    safeSigner
+  );
+
+  const perSecondFee = perYearToPerSecondRate(0.1);
+
+  let txn = await ownable.acceptOwnership();
+  console.log("Waiting to sign:", txn.hash);
+  await txn.wait();
+  txn = await registryDiamond.initializeERC721(
+    "Geo Web Parcel License",
+    "GEOL",
+    ""
+  );
+  console.log("Waiting to sign:", txn.hash);
+  await txn.wait();
+  txn = await registryDiamond.initializeClaimer(
+    1665619570,
+    1666224370,
+    hre.ethers.utils.parseEther("1.0"),
+    hre.ethers.utils.parseEther("0.005"),
+    beaconDiamondAddress
+  );
+  console.log("Waiting to sign:", txn.hash);
+  await txn.wait();
+  txn = await registryDiamond.initializeParams(
+    treasury,
+    ethx.address,
+    sf.host.contract.address,
+    perSecondFee.numerator,
+    perSecondFee.denominator,
+    1,
+    10,
+    60 * 60 * 24 * 7, // 7 days
+    60 * 60 * 24 * 14, // 2 weeks,
+    hre.ethers.utils.parseEther("0.005")
+  );
+  console.log("Waiting to sign:", txn.hash);
+  await txn.wait();
+  console.log("Initialized RegistryDiamond.");
+
+  const beneSuperApp = await deployBeneficiarySuperApp(hre, registryDiamond);
+  console.log("Setting beneficiary to super app...");
+  txn = await registryDiamond.setBeneficiary(beneSuperApp.address);
+  console.log("Waiting to sign:", txn.hash);
+  await txn.wait();
+}
+
+task("deploy:initialize")
+  .addParam("registryDiamondAddress", "RegistryDiamond address")
+  .addParam("beaconDiamondAddress", "BeaconDiamond address")
+  .setAction(
+    async (
+      {
+        registryDiamondAddress,
+        beaconDiamondAddress,
+      }: {
+        registryDiamondAddress: string;
+        beaconDiamondAddress: string;
+      },
+      hre
+    ) => {
+      let sf: Framework;
+      let ethx: SuperToken;
+      if (hre.network.config.chainId == 31337) {
+        const res = await deploySuperfluid(hre);
+        sf = res.sf;
+        ethx = res.ethx;
+      } else {
+        sf = await Framework.create({
+          chainId: hre.network.config.chainId!,
+          provider: hre.ethers.provider,
+        });
+        ethx = await sf.loadSuperToken("ETHx");
+      }
+
+      await initializeRegistryDiamond(
+        hre,
+        sf,
+        ethx,
+        registryDiamondAddress,
+        beaconDiamondAddress
+      );
+    }
+  );
