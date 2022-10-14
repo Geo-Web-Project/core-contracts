11541e58ffc8808d629b9d3ae2c77dd81282e397
Cody Hatfield <cody.hatfield@me.com>
2022-10-14 12:41:10 -0700

    Initial Echidna run

--- contracts/registry/facets/GeoWebParcelFacet.test.sol
+++ contracts/registry/facets/GeoWebParcelFacet.test.sol
@@ -13,3 +13,28 @@ contract TestableGeoWebParcelFacet is GeoWebParcelFacet {
         return LibGeoWebParcel.destroy(id);
     }
 }
+
+contract FuzzyGeoWebParcelFacet is GeoWebParcelFacet {
+    constructor() {
+        uint256[] memory path = new uint256[](1);
+        path[0] = 0;
+        LibGeoWebParcel.build(17179869217, path);
+    }
+
+    function build(uint64 baseCoordinate, uint256[] memory path) external {
+        LibGeoWebParcel.build(baseCoordinate, path);
+    }
+
+    function echidna_coordinate_never_changes_parcel()
+        public
+        view
+        returns (bool)
+    {
+        LibGeoWebParcel.DiamondStorage storage ds = LibGeoWebParcel
+            .diamondStorage();
+
+        LibGeoWebParcel.LandParcel storage p = ds.landParcels[0];
+
+        return p.baseCoordinate == 17179869217;
+    }
+}
