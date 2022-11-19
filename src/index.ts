e39c58b8be587320e6be4b737f42add9b3bd0b0d
Cody Hatfield <cody.hatfield@me.com>
2022-11-19 15:44:32 -0800

    fix: Fix factory exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as IRegistryDiamondABI } from "../abi/contracts/registry/interfaces/IRegistryDiamond.sol/IRegistryDiamond.json";
 export { default as IPCOLicenseDiamondABI } from "../abi/contracts/pco-license/interfaces/IPCOLicenseDiamond.sol/IPCOLicenseDiamond.json";
 
-export { IRegistryDiamond } from "../typechain-types/IRegistryDiamond";
-export { IPCOLicenseDiamond } from "../typechain-types/IPCOLicenseDiamond";
+export { IRegistryDiamond__factory as RegistryDiamondFactory } from "../typechain-types/factories/IRegistryDiamond__factory";
+export { IPCOLicenseDiamond__factory as PCOLicenseDiamondFactory } from "../typechain-types/factories/IPCOLicenseDiamond__factory";
