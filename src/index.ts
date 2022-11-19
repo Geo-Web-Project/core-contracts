0c06315ad1bb300f23237c07a0431ba517425296
Cody Hatfield <cody.hatfield@me.com>
2022-11-19 09:41:51 -0800

    fix: Fix ABI exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { default as IRegistryDiamond } from "../abi/contracts/registry/interfaces/IRegistryDiamond.sol/IRegistryDiamond.json";
-export { default as IPCOLicenseDiamond } from "../abi/contracts/pco-license/interfaces/IPCOLicenseDiamond.sol/IPCOLicenseDiamond.json";
+export { default as IRegistryDiamondABI } from "../abi/contracts/registry/interfaces/IRegistryDiamond.sol/IRegistryDiamond.json";
+export { default as IPCOLicenseDiamondABI } from "../abi/contracts/pco-license/interfaces/IPCOLicenseDiamond.sol/IPCOLicenseDiamond.json";
 
-export { IRegistryDiamond__factory as IRegistryDiamondFactory } from "../typechain-types/factories/IRegistryDiamond__factory";
-export { IPCOLicenseDiamond__factory as IPCOLicenseDiamondFactory } from "../typechain-types/factories/IPCOLicenseDiamond__factory";
+export { IRegistryDiamond } from "../typechain-types/IRegistryDiamond";
+export { IPCOLicenseDiamond } from "../typechain-types/IPCOLicenseDiamond";
